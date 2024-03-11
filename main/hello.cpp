8bb5532fad99931766424268456a58445a6c3e3d
saujanya-sankar <s.saujanya002@gmail.com>
2024-03-11 19:19:39 +0530

    Describe this commit

---
+++ main/hello.cpp
@@ -0,0 +1,11 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "Hello, This is PES2UG21CS444\n"
+  cout << "I have successfully built and run\n";
+  return 0;
+}
