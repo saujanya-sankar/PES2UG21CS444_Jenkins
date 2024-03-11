2ce556196313d8638a9b899b4671467450f50e8d
saujanya-sankar <106611709+saujanya-sankar@users.noreply.github.com>
2024-03-11 19:49:24 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "Hello, This is PES2UG21CS444\n"
+  cout << "Hello, This is PES2UG21CS444\n";
   cout << "I have successfully built and run\n";
   return 0;
 }
